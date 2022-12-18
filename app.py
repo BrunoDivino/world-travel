d4cfad18d05fc037fa0fa2b299953c0294ff2d09
Bruno Divino <brunodivino.dev@gmail.com>
2022-12-18 18:46:16 -0300

    Creating blueprints. A practical way to modularize a flask project

--- app.py
+++ app.py
@@ -1,12 +1,16 @@
-from flask import Flask
+from flask import Flask, Blueprint
 
 # application factory:
 
 def create_app():
     app = Flask(__name__)
 
-    @app.route('/')
+    home = Blueprint('home', __name__)
+
+    @home.route('/')
     def index():
         return 'Hello Flask'
+
+    app.register_blueprint(home)
 
     return app
