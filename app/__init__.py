01cb0c9cfed808a58f623585fd5fcddb9a6615f3
Bruno Divino <brunodivino.dev@gmail.com>
2022-12-18 19:24:32 -0300

    Adding a database configuration

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,18 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 from app import routes
+
+db = SQLAlchemy()
 
 # application factory:
 
 def create_app():
     app = Flask(__name__)
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
     routes.init_app(app)
+    db.init_app(app)
 
     return app
