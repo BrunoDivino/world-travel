55864e7852860d4604036e9ff466b73fcd55b774
Bruno Divino <brunodivino.dev@gmail.com>
2022-12-19 15:53:13 -0300

    Correcting tests

--- __tests__/factories/category_factory.py
+++ __tests__/factories/category_factory.py
@@ -4,12 +4,10 @@ from faker import Faker
 from app.extensions import db
 from app.models import Category
 
-faker = Faker()
-
 class CategoryFactory(factory.alchemy.SQLAlchemyModelFactory):
     class Meta:
         model = Category
         sqlalchemy_session = db.session
         sqlalchemy_session_persistence = 'commit'
 
-    name = faker.text()
+    name = factory.Sequence(lambda x: Faker().name())
