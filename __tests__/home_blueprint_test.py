b3c9a083bae183f91961acf739e57b0260a788de
Bruno Divino <brunodivino.dev@gmail.com>
2022-12-19 11:26:55 -0300

    Creating new post page (blueprint)

--- __tests__/home_blueprint_test.py
+++ __tests__/home_blueprint_test.py
@@ -18,6 +18,16 @@ def _(browser=browser):
     
     assert browser.is_text_present(post.title)
 
+@test('Visitor sees posts')
+def _(browser=browser):
+    post = PostFactory.create()
+
+    browser.visit(url_for('home.index'))
+    browser.links.find_by_text(post.title).click()
+    
+    assert browser.is_text_present(post.title)
+    assert browser.is_text_present(post.text)
+
 @test("Visitor accesses main page and doesn't see posts that aren't published")
 def _(browser=browser):
     published_post = PostFactory.create(title='Enjoying a vacation at Salvador')
