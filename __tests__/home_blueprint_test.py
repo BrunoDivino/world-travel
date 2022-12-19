81d30d60e2f68f96235f89ae40933fa68b72a90f
Bruno Divino <brunodivino.dev@gmail.com>
2022-12-19 02:35:44 -0300

    Using url_for

--- __tests__/home_blueprint_test.py
+++ __tests__/home_blueprint_test.py
@@ -1,4 +1,6 @@
 from ward import test
+from faker import Faker
+from flask import url_for
 
 from app import db
 from app.models import Post
@@ -6,19 +8,24 @@ from __tests__.fixtures import browser
 
 @test('main page must be online')
 def _(browser=browser):
-    browser.visit('/')
+    browser.visit(url_for('home.index'))
     
     assert browser.is_text_present('Hello Flask')
 
 @test('Visitor accesses main page and sees posts')
 def _(browser=browser):
-    post = Post(title='Enjoying a vacation at Salvador', 
+    faker = Faker()
+    post = Post(title=faker.paragraph(), 
                 published=True, 
                 text='text...')
     db.session.add(post)
     db.session.commit()
-
-    browser.visit('/')
+    browser.visit(url_for('home.index'))
     
-    assert browser.is_text_present('Enjoying a vacation at Salvador')
+    assert browser.is_text_present(post.title)
     
+@test("Visitor can't see any posts")
+def _(browser=browser):
+    browser.visit(url_for('home.index'))
+    
+    assert browser.is_text_present('No post has been found')
